02a9e60ca348316c7e93260125408e951ad654a5
Cristina Coxinho <mcncoxinho@gmail.com>
2022-06-04 16:27:05 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DNA_DuplaHelice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
